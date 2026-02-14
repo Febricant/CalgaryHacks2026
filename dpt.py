732bc43b93374ca6c0a1b4b35d5cf6022b555c0b
derpytim <jacob.bob.mann21@gmail.com>
2026-02-14 11:17:44 -0700

    67 67

--- dpt.py
+++ dpt.py
@@ -1,11 +1,12 @@
 import random
 
+correct_number = random.randint(1, 67)
+
 while True:
-    correct_number = random.randint(1, 67)
     input_number = int(input("Guess the number between 1 and 67: "))
 
     if input_number == correct_number:
         print("!!")
-
+        break
     else:
-        print("??")
+        print("??"))
